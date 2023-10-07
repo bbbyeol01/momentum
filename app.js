81071a02efeeeb4a45c06a23f4f8d4cf0caa0d11
bbbyeol <forbyeol@naver.com>
2023-10-07 14:27:33 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const calculater = {
+    minus: function(num1, num2) {
+        console.log(num1 - num2);
+    },
+
+    plus: function(num1, num2) {
+
+        console.log(num1 + num2);
+    },
+
+    divide: function(num1, num2) {
+        console.log(num1 / num2);
+    },
+
+    multyply: function(num1, num2) {
+        console.log(num1 * num2);
+    },
+
+    powerOf: function(num1, num2) {
+        console.log(num1 ** num2);
+    }
+}
+
+calculater.powerOf(2, 10);
+calculater.minus(5, 10);
