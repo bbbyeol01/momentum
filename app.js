82627e4da79083c794f68f1f5e7b6faead14a55f
bbbyeol <forbyeol@naver.com>
2023-10-09 17:45:04 +0900

    유저정보 localStorage에 저장

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault(); //  submit되면 새로고침되는 브라우저의 기본 값을 막음.
+    event.preventDefault(); 
+    //  submit되면 새로고침되는 브라우저의 기본 값을 막음.
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello, ${savedUsername}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello, ${username}` ;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
