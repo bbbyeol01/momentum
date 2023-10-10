58a6ea974491a3bc15bb885a49a552bf359fca68
bbbyeol <forbyeol@naver.com>
2023-10-10 15:32:39 +0900

    todo 삭제 구현

--- JS/todo.js
+++ JS/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoList = document.querySelector("#todo-list");
 const toDoInput = document.querySelector("#todo-form input");
 
+function deleteToDo(event) {
+    const lili = event.target.parentElement;
+    lili.remove();
+}
+
 function paintToDo(newTodo){
     const lili = document.createElement("li");
     const spanspan = document.createElement("span");
-    lili.appendChild(spanspan);
     spanspan.innerText = newTodo;
+    const buttonbutton = document.createElement("button");
+    buttonbutton.innerText = "❌";
+    buttonbutton.addEventListener("click", deleteToDo);
+    lili.appendChild(spanspan);
+    lili.appendChild(buttonbutton);
     toDoList.appendChild(lili);
 }
 
