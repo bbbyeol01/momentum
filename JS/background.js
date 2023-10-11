1a53d495bd0270ba5a466407b55982b00afdef89
bbbyeol <forbyeol@gmail.com>
2023-10-11 10:34:06 +0900

    js가 아닌 css내에서 배경 속성 변경하도록 수정

--- JS/background.js
+++ JS/background.js
@@ -9,9 +9,3 @@ const images = [
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 document.body.style.backgroundImage = `url(img/${chosenImage})`;
-document.body.style.backgroundPosition = "top";
-document.body.style.backgroundRepeat = "no-repeat";
-document.body.style.backgroundSize = "cover";
-document.body.style.backgroundAttachment = "fixed";
-
-document.body.appendChild(bgImage);
